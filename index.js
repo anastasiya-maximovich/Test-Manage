805759324f323c431831ff9e38dba2dce5b614bf
anastasiya025 <anastasiya025.am@gmail.com>
2021-05-09 02:10:52 +0300

    feat: create content part

---
+++ index.js
@@ -0,0 +1,20 @@
+/* const visibleSort = document.querySelectorAll('.slider-list')
+      major = document.querySelector('.btn-major'),
+      minor = document.querySelector('.btn-minor');
+
+visibleSort.forEach((el) => {
+  el.addEventListener('click', () => {
+    el.classList.toggle('active')
+    })
+})
+
+major.addEventListener('click', () =>{
+  minor.classList.remove('btn-active')
+  major.classList.toggle('btn-active')
+})
+
+minor.addEventListener('click', () =>{
+  major.classList.remove('btn-active')
+  minor.classList.toggle('btn-active')
+})
+*/
